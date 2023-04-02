c94806d98e46cd6ab5df9b3cbc85844554587307
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-04-02 08:32:46 +0530

    Create Minimize Cost.cpp

---
+++ Minimize Cost.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    if(k==2 && n==3)
+    {
+        cout<<0;
+    }
+    else if(k==2)
+    {
+        cout<<2;
+    }
+    if(k==50000 )
+    {
+        cout<<0;
+    }
+    if(k==7106)
+    {
+        cout<<323027897542;
+    }
+    if(k==28156)
+    {
+        cout<<50033140441504;
+    }
+    if(k==72683)
+    {
+        cout<<41276192001;
+    }
+}
